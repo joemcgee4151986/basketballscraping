28e2b0b40a1cff529fda7c5537a531e6b9b719f0
unknown <jrmcgee@asu.edu>
2022-05-16 15:39:51 +0900

    updated project with more code

--- components/search.js
+++ components/search.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 import {connect} from 'react-redux';
 import styled from 'styled-components';
 
-import {get Player} from '../store/actions/player';
+import {getPlayer} from '../store/actions/player';
 
 class Search extends Component {
 
@@ -18,7 +18,17 @@ searchPlayer = (e) => {
     if (firstName && lastName) {
         // get the players first and last name
         this.props.getPlayer(firstName, lastName);
+    } else{
+        this.setState({error: 'Search requires a first and last name'});
     }
 }
-
+handleChange = (e) => {
+    this.setState({searchTerm: e.target.value})
+}
+render(){
+    const {seachTerm, error} = this.state;
+    return (
+        <form onSubmit={this.searchPlayer}></form>
+    )
+}
 }
