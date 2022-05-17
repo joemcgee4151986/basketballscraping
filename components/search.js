65854b969a2c2cb188da9ead6eee163b3a2f47c1
unknown <jrmcgee@asu.edu>
2022-05-17 15:40:30 +0900

    added more code

--- components/search.js
+++ components/search.js
@@ -28,7 +28,49 @@ handleChange = (e) => {
 render(){
     const {seachTerm, error} = this.state;
     return (
-        <form onSubmit={this.searchPlayer}></form>
+        <form onSubmit={this.searchPlayer}>
+            <SearchWrapper>
+                <SearchBar type = "text" value{searchTerm} onChange={this.handleChange} />
+                <SearchButton type ="submit"></SearchButton>
+            </SearchWrapper>
+            {error && <Error>{error}</Error>}
+        </form>
     )
+
 }
 }
+
+export default connect(null, {getPlayer})(Search);
+
+const SearchWrapper = styled.div`
+
+display: flex;
+flex-direction: row;
+width: 80%;
+width:600px;
+margin: 20px auto;
+`
+
+const SearchBar = styled.input`
+padding: 13px 20px;
+border: 1.2px solid white;
+color: gray;
+font-size: 17px;
+width: 260px;
+border-radius: 7px 0px 0px 5px;
+flex: 6;
+`
+const SearchButton = styled.button`
+background-color: #2F5EE5;
+padding:14px 20px;
+border:none;
+border-radius:0px 5px 5px 0px;
+cursor: pointer;
+flex: 2;
+`
+
+const Error = styled.span`
+    color: red;
+    padding:4px;
+    display:block;
+    font-size 11px;`
